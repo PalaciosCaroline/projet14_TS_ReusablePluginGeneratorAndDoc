3aeef16c8da436e05aeefa5addfcc2a232b92231
PalaciosCaroline <caroline2807@yahoo.fr>
2023-04-10 17:38:08 +0200

    installation de lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import React from "react";
-import NewEmployee from './pages/NewEmployee';
-import { BrowserRouter,Routes, Route} from "react-router-dom";
-import ListEmployees from './pages/ListEmployees';
-import Home from "./pages/Home";
+// import React, { lazy, Suspense } from 'react';
+// import NewEmployee from './pages/NewEmployee';
+// import { BrowserRouter,Routes, Route} from "react-router-dom";
+// import ListEmployees from './pages/ListEmployees';
+// import Home from "./pages/Home";
+
+
+
+
+// const Home = lazy(() => import('./pages/Home'));
+// const NewEmployee = lazy(() => import('./pages/NewEmployee'));
+// const ListEmployees = lazy(() => import('./pages/ListEmployees'));
+
+// export default function App() {
+// document.title = 'HRnet';
+
+// return(
+//     <BrowserRouter>
+//      <Suspense fallback={<div>Loading...</div>}>
+//         <Routes>
+//           <Route index path="/" element={<Home />} />
+//           <Route path="/newemployee" element={<NewEmployee />} />
+//           <Route path="/listemployees" element={<ListEmployees />} />
+//         </Routes>
+//       </Suspense>
+//     </BrowserRouter>
+//   )
+// }
+
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home'));
+const NewEmployee = lazy(() => import('./pages/NewEmployee'));
+const ListEmployees = lazy(() => import('./pages/ListEmployees'));
 
 export default function App() {
-document.title = 'HRnet';
+  document.title = 'HRnet';
 
-return(
+  return (
     <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route index path="/" element={<Home />} />
           <Route path="/newemployee" element={<NewEmployee />} />
           <Route path="/listemployees" element={<ListEmployees />} />
         </Routes>
+      </Suspense>
     </BrowserRouter>
-  )
+  );
 }
-
