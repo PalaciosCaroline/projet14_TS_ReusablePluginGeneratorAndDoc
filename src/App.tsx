92e9c5c92cffb8661601794ae418ac698cf4e956
PalaciosCaroline <caroline2807@yahoo.fr>
2023-04-10 17:39:48 +0200

    nettoyage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-// import React, { lazy, Suspense } from 'react';
+// import React from "react";
 // import NewEmployee from './pages/NewEmployee';
 // import { BrowserRouter,Routes, Route} from "react-router-dom";
 // import ListEmployees from './pages/ListEmployees';
 // import Home from "./pages/Home";
-
-
-
-
-// const Home = lazy(() => import('./pages/Home'));
-// const NewEmployee = lazy(() => import('./pages/NewEmployee'));
-// const ListEmployees = lazy(() => import('./pages/ListEmployees'));
 
 // export default function App() {
 // document.title = 'HRnet';
 
 // return(
 //     <BrowserRouter>
-//      <Suspense fallback={<div>Loading...</div>}>
 //         <Routes>
 //           <Route index path="/" element={<Home />} />
 //           <Route path="/newemployee" element={<NewEmployee />} />
 //           <Route path="/listemployees" element={<ListEmployees />} />
 //         </Routes>
-//       </Suspense>
 //     </BrowserRouter>
 //   )
 // }
