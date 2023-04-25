1aa59ed7a1089a2fc10429f5cb11746848a716f2
PalaciosCaroline <caroline2807@yahoo.fr>
2023-04-25 21:52:35 +0200

    rendu

--- src/components/DateOfBirth.tsx
+++ src/components/DateOfBirth.tsx
@@ -1,60 +1,6 @@
-// import React from 'react'
-// import { useDispatch} from 'react-redux';
-// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-// import dayjs from 'dayjs';
-// import {
-//     setField,
-//     setError
-//   } from '../store/newEmployeeEntreeSlice';
-
-//   interface DateOfBirthProps {
-//     errordateOfBirth: string;
-//   }
-
-// export default function DateOfBirth({ errordateOfBirth }: DateOfBirthProps): JSX.Element {
-//     const dispatch = useDispatch();
-//     const noBeforeDay = dayjs().subtract(90, 'year');
-//     const noAfterDay = dayjs().subtract(12, 'year');
-
-//     const handleDateChange = (date: Date | null): void => {
-//         dispatch(setError({name:'dateOfBirth', message:'' }))
-//         const isInvalid = dayjs(noBeforeDay).isAfter(date) || dayjs(noAfterDay).isBefore(date);
-//         if(isInvalid){
-//             dispatch(setError({name:'dateOfBirth', message:'Please select a valid date ' }))
-//             return;
-//         }else {
-//             dispatch(setError({name:'dateOfBirth', message:'' }))
-//             dispatch(setField({ name:'dateOfBirth', value: dayjs(date).format('DD/MM/YYYY') }));        
-//         }
-//     };
-   
-//   return (
-//     <div className='form-group box_dateOfBirth'>       
-//         <p>Date of Birth</p>
-//         <div className='box_Input ' style={{display:'flex', position:'relative'}}>
-//         <DatePicker 
-//         label="Date of Birth Select" 
-//         minDate={noBeforeDay.toDate()}
-//         maxDate={noAfterDay.toDate()}
-//         views={['year', 'month', 'day']}
-//         onChange={handleDateChange}
-//         sx={{
-//           '& .MuiInputBase-input': {
-//             height: '25px',
-//             width: '160px', 
-//             margin:'10px',
-//           },
-//         }}
-//         />
-//          {errordateOfBirth ? <p className='errorMessage'>{errordateOfBirth}</p> : ''}
-//         </div>
-//     </div>
-//   )
-// }
-
-
-import React, {Dispatch,SetStateAction } from 'react';
-import { useDispatch} from 'react-redux';
+import React, {Dispatch,SetStateAction, useEffect } from 'react';
+import { RootState } from './../store/index';
+import { useDispatch, useSelector} from 'react-redux';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
 import {
@@ -73,10 +19,16 @@ import {
   }
 
 export default function DateOfBirth({errordateOfBirth,initialValues, setInitialValues}: DateOfBirthProps): JSX.Element {
-// export default function DateOfBirth({errordateOfBirth}) {
+  const dateOfBirth = useSelector((state: RootState) => state.newEmployeeEntree.dateOfBirth);
     const dispatch = useDispatch();
     const noBeforeDay = dayjs().subtract(90, 'year');
     const noAfterDay = dayjs().subtract(12, 'year');
+
+    useEffect(() => {
+      if (!dateOfBirth || !initialValues.dateOfBirthInput) {
+        dispatch(setError({ name: 'dateOfBirth' , message: '' }))
+        }
+    }, []);
 
     const handleDateChange = (date : any): void => {
         dispatch(setError({name:'dateOfBirth', message:'' }))
--- src/components/StartDate.tsx
+++ src/components/StartDate.tsx
@@ -1,6 +1,7 @@
-import React, {Dispatch,SetStateAction } from 'react';
-import { useDispatch } from 'react-redux';
+import React, {Dispatch,SetStateAction, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { RootState } from './../store/index';
 import dayjs from 'dayjs';
 import {
   setField,
@@ -19,8 +20,15 @@ interface StartDateProps {
 
 export default function StartDate({ errorstartDate, initialValues, setInitialValues}: StartDateProps): JSX.Element {
   const dispatch = useDispatch();
+  const startDate = useSelector((state: RootState) => state.newEmployeeEntree.startDate);
   const noBeforeDay = dayjs().subtract(1, 'year');
   const noAfterDay = dayjs().add(1, 'year');
+
+  useEffect(() => {
+    if (!startDate || !initialValues.startDateInput) {
+      dispatch(setError({ name: 'startDate' , message: '' }))
+      }
+  }, []);
 
    const handleDateChange = (date: any): void => {
     setInitialValues({ ...initialValues, startDateInput: date }); 
