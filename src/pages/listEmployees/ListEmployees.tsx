884afc14a1042bea3106a0b5bf91366574fa671a
PalaciosCaroline <caroline2807@yahoo.fr>
2023-06-11 22:04:34 +0200

    nettoyage

--- src/pages/listEmployees/ListEmployees.tsx
+++ src/pages/listEmployees/ListEmployees.tsx
@@ -5,21 +5,6 @@ import { Header } from '../../components/Header';
 import { linkNewEmployee } from './../../utils/linkData';
 import { RootState } from '../../store/index';
 
-
-// export default function ListEmployees(): JSX.Element {
-//   const employees = useSelector((state: any) => state.employees);
-
-//   return (
-//     <>
-//       <Header linkProps={linkNewEmployee} />
-
-//       <main className="main_ListEmployees" data-testid="listEmployees">
-//         <TableEmployees employees={employees} />
-//       </main>
-//     </>
-//   );
-// }
-
 export default function ListEmployees(): JSX.Element {
   const employees = useSelector((state: RootState) => state.employees.active);
   return (
